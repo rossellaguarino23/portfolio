cc4eba71ff935bf0efbdeba82c98729b1467bce5
Rossella <rossg.design@gmail.com>
2026-02-02 19:01:48 +0100

    mucho trabajo

---
+++ src/app/components/approach/approach.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approach',
+  imports: [],
+  templateUrl: './approach.component.html',
+  styleUrl: './approach.component.scss'
+})
+export class ApproachComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { AboutComponent } from '../../components/about/about.component';
 import { CaseStudiesComponent } from '../../components/case-studies/case-studies.component';
 import { HelloComponent } from '../../components/hello/hello.component';
 import { SkillCarouselComponent } from '../../components/skill-carousel/skill-carousel.component';
+import { ApproachComponent } from "../../components/approach/approach.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HelloComponent, CaseStudiesComponent, SkillCarouselComponent, AboutComponent],
+  imports: [HelloComponent, CaseStudiesComponent, SkillCarouselComponent, ApproachComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
