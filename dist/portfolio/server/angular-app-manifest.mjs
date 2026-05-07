deda28ba8f7c5bef0258c02cba89712c2442dbd1
Haakjvork <haakjvork@gmail.com>
2026-05-07 10:48:07 +0200

    build-prod 1.0.5

--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -8,32 +8,32 @@ export default {
   entryPointToBrowserMapping: {
   "src/app/pages/home/home.component.ts": [
     {
-      "path": "chunk-PVF3HEMX.js",
+      "path": "chunk-RP4PIYZQ.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/bladder-boss/bladder-boss.component.ts": [
     {
-      "path": "chunk-RB75UEU3.js",
+      "path": "chunk-IBII5KXX.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/boardmatch/boardmatch.component.ts": [
     {
-      "path": "chunk-RLKYYGY6.js",
+      "path": "chunk-IS3EMGMX.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/avolta/avolta.component.ts": [
     {
-      "path": "chunk-M3V66R2A.js",
+      "path": "chunk-BC7COHQ2.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 14153, hash: '64a857b4b361b64f811c5f107c84c55bd526ded4cbbcdf1adfee0050a526e238', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 9444, hash: '9bf0ee0f46749f558a7de3de4d9d93c76d9f63cf6aaf0fc3d83ad717fa69587b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-DI2RXZ3F.css': {size: 242068, hash: 'f5Y0KXxJ8cQ', text: () => import('./assets-chunks/styles-DI2RXZ3F_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7462, hash: '635c044217f1cf20214fdb8607dfa0fa1f5a53f73b59ec70768eff59a45640bc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2754, hash: '0257191e4d575cebb03933b709a452433c302e4adc4f052dff30b50cc9bbe07b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-MYKLKJGO.css': {size: 242108, hash: 'RSYQjMwZM4o', text: () => import('./assets-chunks/styles-MYKLKJGO_css.mjs').then(m => m.default)}
   },
 };
