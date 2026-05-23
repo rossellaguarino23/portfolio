5b1243ebf8aabf5bb630d44f51562f4abc17b91a
Rossella <rossg.design@gmail.com>
2026-05-23 13:06:02 +0200

    accessability and taxeezy bug fixed

--- src/app/pages/case-studies/taxeezy/taxeezy.component.ts
+++ src/app/pages/case-studies/taxeezy/taxeezy.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GalleryModalComponent } from '../../../components/gallery-modal/gallery-modal.component';
 
 @Component({
   selector: 'app-taxeezy',
-  imports: [],
+  imports: [GalleryModalComponent],
   templateUrl: './taxeezy.component.html',
   styleUrl: './taxeezy.component.scss'
 })
 export class TaxeezyComponent {
+    @ViewChild(GalleryModalComponent) galleryModal!: GalleryModalComponent;
+
+  galleryImages: string[] = [
+    '/case-studies/BladderBoss-1.png',
+    '/case-studies/BladderBoss-2.png',
+    '/case-studies/BladderBoss-3.png',
+    '/case-studies/BladderBoss-Home.png',
+    '/case-studies/BladderBoss-Diary.png',
+    '/case-studies/BladderBoss-Profile.png',
+  ];
+
+  selectedImage: string | null = null;
+
+  openModal(image: string): void {
+    this.selectedImage = image;
+    this.galleryModal.openModal(image);
+  }
 
 }
