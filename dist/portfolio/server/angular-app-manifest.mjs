b1a00e64c4b37725ca0dfcf3a94b6b50ca6fb386
Rossella <rossg.design@gmail.com>
2026-06-01 12:33:05 +0200

    build prod 1.0.7

--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -8,32 +8,46 @@ export default {
   entryPointToBrowserMapping: {
   "src/app/pages/home/home.component.ts": [
     {
-      "path": "chunk-XCBGNHSP.js",
+      "path": "chunk-POLDZDDF.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/bladder-boss/bladder-boss.component.ts": [
     {
-      "path": "chunk-IBII5KXX.js",
+      "path": "chunk-5BKEXWSS.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-YYY6ZTZI.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/boardmatch/boardmatch.component.ts": [
     {
-      "path": "chunk-IS3EMGMX.js",
+      "path": "chunk-KEHD4KW3.js",
       "dynamicImport": false
     }
   ],
   "src/app/pages/case-studies/avolta/avolta.component.ts": [
     {
-      "path": "chunk-BC7COHQ2.js",
+      "path": "chunk-N24WSP6X.js",
+      "dynamicImport": false
+    }
+  ],
+  "src/app/pages/case-studies/taxeezy/taxeezy.component.ts": [
+    {
+      "path": "chunk-NTZ6SZWM.js",
+      "dynamicImport": false
+    },
+    {
+      "path": "chunk-YYY6ZTZI.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 7462, hash: 'e40853d1ec5c4008aa6854da9b9cefa43098b176b93b31837bc951983e48ae0c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2754, hash: '31f10594e0b313e6e439b1a714e0148cb8c6b9521571d1f1a1fc3d49fcb53f48', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-MYKLKJGO.css': {size: 242108, hash: 'RSYQjMwZM4o', text: () => import('./assets-chunks/styles-MYKLKJGO_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7482, hash: 'd0332679101644388af01e4f83da4af05043d8a2e271c48cb35640c14af5a24e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2754, hash: '404ac411c478220420c4cdbf45806d6b3c4b9d1580af26b55698ec75e365e824', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-662477QQ.css': {size: 248250, hash: 'LC5nebXTXns', text: () => import('./assets-chunks/styles-662477QQ_css.mjs').then(m => m.default)}
   },
 };
