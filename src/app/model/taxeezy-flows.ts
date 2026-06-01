576ec8ac60a4749ebcbe9a71ffbdf7ec24c5b8ff
Rossella <rossg.design@gmail.com>
2026-06-01 12:31:45 +0200

    final wireframes flow

--- src/app/model/taxeezy-flows.ts
+++ src/app/model/taxeezy-flows.ts
@@ -1,33 +1,33 @@
 import { InteractiveFlow } from '../types/gallery';
 
 export const utrFlow: InteractiveFlow = {
-  title: 'Taxeezy - UTR Setup Flow',
+  title: 'Taxeezy - UTR Registration Flow',
   key: 'taxeezy-UTR',
   startScreenId: '1',
   screens: {
     '1': {
       id: '1',
-      hotspots: [{ x: 26, y: 73, width: 10, height: 6, targetScreenId: '2', tooltip: 'Open Accordion' }],
+      hotspots: [{ x: 31, y: 75.5, width: 0, height: 0, targetScreenId: '2', tooltip: 'Open Accordion' }],
     },
     '2': {
       id: '2',
-      hotspots: [{ x: 64, y: 45.5, width: 10, height: 7, targetScreenId: '3', tooltip: 'Go Identiy Verification' }],
+      hotspots: [{ x: 69, y: 49, width: 0, height: 0, targetScreenId: '3', tooltip: 'Go Identiy Verification' }],
     },
     '3': {
       id: '3',
-      hotspots: [{ x: 64, y: 45.5, width: 10, height: 7, targetScreenId: '4', tooltip: 'Start Identity Verification' }],
+      hotspots: [{ x: 68, y: 52.5, width: 0, height: 0, targetScreenId: '4', tooltip: 'Start Identity Verification' }],
     },
     '4': {
       id: '4',
-      hotspots: [{ x: 64, y: 45.5, width: 10, height: 7, targetScreenId: '5', tooltip: 'Continue' }],
+      hotspots: [{ x: 92, y: 71.5, width: 0, height: 0, targetScreenId: '5', tooltip: 'Continue' }],
     },
     '5': {
       id: '5',
-      hotspots: [{ x: 64, y: 45.5, width: 10, height: 7, targetScreenId: '6', tooltip: 'Continue' }],
+      hotspots: [{ x: 92, y: 38.5, width: 0, height: 0, targetScreenId: '6', tooltip: 'Continue' }],
     },
     '6': {
       id: '6',
-      hotspots: [{ x: 64, y: 45.5, width: 10, height: 7, targetScreenId: '7', tooltip: 'Back to home' }],
+      hotspots: [{ x: 50.5, y: 38.5, width: 0, height: 0, targetScreenId: '7', tooltip: 'Back to home' }],
     },
     '7': {
       id: '7',
@@ -37,17 +37,17 @@ export const utrFlow: InteractiveFlow = {
 };
 
 export const dashboardFlow: InteractiveFlow = {
-  title: 'Taxeezy - UTR Setup Flow',
+  title: 'Taxeezy - Dashboard Info and Document Upload Flow',
   key: 'taxeezy-dashboard',
   startScreenId: '1',
   screens: {
     '1': {
       id: '1',
-      hotspots: [{ x: 72, y: 68, width: 12, height: 6, targetScreenId: '2', tooltip: 'Open Accordion' }],
+      hotspots: [{ x: 85.7, y: 70, width: 0, height: 0, targetScreenId: '2', tooltip: 'Open Accordion' }],
     },
     '2': {
       id: '2',
-      hotspots: [{ x: 72, y: 68, width: 12, height: 6, targetScreenId: '3', tooltip: 'Go Identiy Verification' }],
+      hotspots: [{ x: 75, y: 74, width: 12, height: 6, targetScreenId: '3', tooltip: 'Go Identiy Verification' }],
     },
     '3': {
       id: '3',
@@ -63,32 +63,27 @@ export const profileFlow: InteractiveFlow = {
   screens: {
     '1': {
       id: '1',
-      hotspots: [
-        { x: 92, y: 55, width: 5, height: 5, targetScreenId: '2', tooltip: 'Open Accordion' },
-        { x: 3, y: 16, width: 5, height: 5, targetScreenId: '3', tooltip: 'Go Identify Verification' },
-      ],
+      hotspots: [{ x: 94, y: 93.5, width: 5, height: 5, targetScreenId: '2', tooltip: 'Save' }],
     },
     '2': {
       id: '2',
-      hotspots: [
-        { x: 0, y: 8, width: 5, height: 5, targetScreenId: '3', tooltip: 'Go Identiy Verification' },
-        { x: 92, y: 92, width: 5, height: 5, targetScreenId: '3', tooltip: 'Go Identify Verification' },
-      ],
+      hotspots: [{ x: 70, y: 70, width: 5, height: 5, targetScreenId: '3', tooltip: 'Start Verification' }],
     },
     '3': {
       id: '3',
-      hotspots: [{ x: 68, y: 68, width: 5, height: 5, targetScreenId: '4', tooltip: 'Start Identity Verification' }],
+      hotspots: [{ x: 72.5, y: 53, width: 5, height: 5, targetScreenId: '4', tooltip: 'Continue Verification' }],
     },
     '4': {
       id: '4',
-      hotspots: [
-        { x: 6, y: 16, width: 5, height: 5, targetScreenId: '3', tooltip: 'Go Identiy Verification' },
-        { x: 70, y: 51, width: 5, height: 5, targetScreenId: '5', tooltip: 'Continue Identity Verification' },
-      ],
+      hotspots: [{ x: 71, y: 54, width: 5, height: 5, targetScreenId: '5', tooltip: 'Continue Identity Verification' }],
     },
     '5': {
       id: '5',
-      hotspots: [{ x: 70, y: 51, width: 5, height: 5, targetScreenId: '1', tooltip: 'Submit Identity Verification' }],
+      hotspots: [{ x: 50, y: 39, width: 5, height: 5, targetScreenId: '6', tooltip: 'Continue Identity Verification' }],
+    },
+    '6': {
+      id: '6',
+      hotspots: [],
     },
   },
 };
